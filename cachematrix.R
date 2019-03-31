47796780c2889f321f15bc95ddd2f51c37b8f81c
Renee Noguera <rgnoguera@globe.com.ph>
2019-03-31 14:15:20 +0800

    changes from local computer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two function computes for an inverse of a square matrix (a square matrix is always invertible)
+## The first function caches the matrix, then the second function solves for the inverse of the cached matrix
 
-## Write a short comment describing this function
 
+## For the function to solve for the inverse, I used the inv() function from the matlib package. 
+## You can install the matlib package or just change the inv() function under cacheSolve to solve() which does the same thing.
+
+library(matlib)
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function solves for the inverse of the cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cache of inverse data")
+    return(m)
+  }
+  data <- x$get()
+  m <- inv(data, ...)
+  x$setinverse(m)
+  m
 }
